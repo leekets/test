89e8ed371a1ff2d8ad822c648760aceceb079e4c
Leeke <Leeke@Leeke-PC>
2012-05-28 17:06:49 +0800

    improve select js

--- application/views/js/section/v2/s_v2_functions.js
+++ application/views/js/section/v2/s_v2_functions.js
@@ -1,21 +1,19 @@
-
-	function Arrsplit(s){
-		//s1=0+(s-1)*10;
-		//alert(s);
-		var x=5;
-		var comments2=[];
-		for(var i=(s-1)*x, len=s*x; i<len; i++){
-			comments2.push(comments[i]);	
-		}
-		comments = comments2;				//题库赋值错误列表
-		errors = [];					//清空：错误列表；
-		index = 0;						//初始化：指针
-		eNum = 0;						//初始化：错误计数
-		$("span.sp3").html('');			//清空：页面错误计数	
-		act();							//运行函数；	
-		$(".f_slipt").hide();
-	}
-
+function Arrsplit(s) {
+    //s1=0+(s-1)*10;
+    //alert(s);
+    var x = 5;
+    var comments2 = [];
+    for (var i = (s - 1) * x, len = s * x; i < len; i++) {
+        comments2.push(comments[i]);
+    }
+    comments = comments2; //题库赋值错误列表
+    errors = []; //清空：错误列表；
+    index = 0; //初始化：指针
+    eNum = 0; //初始化：错误计数
+    $("span.sp3").html(''); //清空：页面错误计数	
+    act(); //运行函数；	
+    $(".f_slipt").hide();
+}
 
 //		function sen1(a){
 //			
@@ -30,68 +28,79 @@
 //
 //
 //		}
+function play3(i) {
 
-		function play3(i){
-			
-			if(i==1){
-				audio.currectTime=0;
-				audio.play();
-				i=i+1;
-//				alert("123");
-//				playVar="play3("+i+");";
-				setTimeout("play3(2)",2000);
-//				play3(i);
-//			}else{
-//				return false;
-			}else if(i==2){
-//				alert("123123");
-				audio.currectTime=0;
-				audio.play();
-				i=i+1;
-//				alert("123");
-//				playVar="play3("+i+");";
-				setTimeout("play3(3)",2000);				
-			}else if(i==3){
-				audio.currectTime=0;
-				audio.play();
-				i=i+1;
-//				alert("123");
-//				playVar="play3("+i+");";
-				setTimeout("play3(4)",2000);				
-			}else if(i==4){
-				audio.currectTime=0;
-				audio.play();
-				i=i+1;
-//				alert("123");
-//				playVar="play3("+i+");";
-//				setTimeout("play3(4)",2000);				
-			}
+    if (i == 1) {
+        audio.currectTime = 0;
+        audio.play();
+        i = i + 1;
+        setTimeout("play3(2)", 2000);
+    } else if (i == 2) {
+        audio.currectTime = 0;
+        audio.play();
+        i = i + 1;
+        setTimeout("play3(3)", 2000);
+    } else if (i == 3) {
+        audio.currectTime = 0;
+        audio.play();
+        i = i + 1;
+        setTimeout("play3(4)", 2000);
+    } else if (i == 4) {
+        audio.currectTime = 0;
+        audio.play();
+        i = i + 1;
+    }
 
-		}
-		
-//todo:转存如单页js
-		function MillisecondToDate(msd)
-		{
-			var time = parseFloat(msd) / 1000;
-			if (null != time && "" != time)
-			{
-				if (time > 60 && time < 60 * 60)
-				{
-					time = parseInt(time / 60.0) + "分钟" + parseInt((parseFloat(time / 60.0) -
-							parseInt(time / 60.0)) * 60) + "秒";
-				}
-				else if (time >= 60 * 60 && time < 60 * 60 * 24)
-				{
-					time = parseInt(time / 3600.0) + "小时" + parseInt((parseFloat(time / 3600.0) -
-							parseInt(time / 3600.0)) * 60) + "分钟" +
-							parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
-									parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) + "秒";
-				}
-				else
-				{
-					time = parseInt(time) + "秒";
-				}
-			}
-			return time;
-		}
+}
 
+//计算总时间
+function MillisecondToDate(msd) {
+    var time = parseFloat(msd) / 1000;
+    if (null != time && "" != time) {
+        if (time > 60 && time < 60 * 60) {
+            time = parseInt(time / 60.0) + "分钟" + parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60) + "秒";
+        } else if (time >= 60 * 60 && time < 60 * 60 * 24) {
+            time = parseInt(time / 3600.0) + "小时" + parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) + "分钟" + parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) - parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) + "秒";
+        } else {
+            time = parseInt(time) + "秒";
+        }
+    }
+    return time;
+}
+
+//随机改变数组的排序
+function randomOrder(targetArray) {
+    var arrayLength = targetArray.length
+    var tempArray1 = new Array(); //先创建一个正常顺序的数组
+    for (var i = 0; i < arrayLength; i++) {
+        tempArray1[i] = i
+    }
+    var tempArray2 = new Array();	//再根据上一个数组创建一个随机乱序的数组
+    for (var i = 0; i < arrayLength; i++) {
+        tempArray2[i] = tempArray1.splice(Math.floor(Math.random() * tempArray1.length), 1)	//从正常顺序数组中随机抽出元素
+    }
+    var tempArray3 = new Array();	//最后创建一个临时数组存储 根据上一个乱序的数组从targetArray中取得数据
+    for (var i = 0; i < arrayLength; i++) {
+        tempArray3[i] = targetArray[tempArray2[i]]
+    }
+    return tempArray3	//返回最后得出的数组
+    //使用实例
+    // var tmp = ["1", "2", "3", "4"];
+    // alert(randomOrder(tmp));
+}
+
+//打印数组
+function print_array(arr) {
+    var t = 'array(\n';
+    for (var key in arr) {
+        if (typeof(arr[key]) == 'array' || typeof(arr[key]) == 'object') {
+            var t_tmp = key + ' = ' + print_array(arr[key]);
+            t += '\t' + t_tmp + '\n';
+        } else {
+            var t_tmp = key + ' = ' + arr[key];
+            t += '\t' + t_tmp + '\n';
+        }
+    }
+    t = t + ')';
+    return t;;
+}
--- application/views/js/t_js_testSelect.php
+++ application/views/js/t_js_testSelect.php
@@ -1,14 +1,30 @@
-	<?php 
-	$a="1";
-	if($a=="123"){?>
-	<script>
-	<?php }?>
+	<?php $a="1"; if($a=="123"){?> <script> <?php }?>
 	
 $(function() {
 	
+	//错误返回按钮延时时间；
+	var dNum= 4000;	
+	var testSize=20;
+
 	var t1= new Date().getTime(); //初始化时间
-	var dNum= 2000;	//错误返回按钮延时时间；
-	var testSize=0;
+    var index = 0;	//数组指针初始化；
+	//del:120528
+//	var index_ = "";
+//	var errors_1st = [];
+//	var round=1;
+	var getError="";
+	var errors = [];	//错误返回按钮延时时间；
+	var eNum = 0;	//错误计数初始化；
+	$("#ul").show();	//显示全局
+	//调试开关；
+	var debug="0";
+	if(debug=="1"){
+		dNum=0;	
+	}
+	
+	var org_comments=[];
+	org_comments=comments;
+	comments=randomOrder(comments);
 	if(testSize>0){
 		var comments2 = [];
 		for(i=0;i<=testSize-1;i++){
@@ -16,32 +32,8 @@ $(function() {
 		}
 		comments = comments2;
 	}
-	
-	//数组指针初始化；
-    var index = 0;
-	var index_ = "";
-	var errors_1st = [];
-	var round=1;
-	var getError="";
-		
-	var errors = [];	//错误返回按钮延时时间；
-	var eNum = 0;	//错误计数初始化；
-	
-	//显示全局
-	$("#ul").show();
-	
-	//调试开关；
-	var debug="0";
-	if(debug=="1"){
-		dNum=0;	
-	}
-	
-	
-	var org_comments=[];
-	org_comments=comments;
-	comments=randomOrder(comments);
 	act(); //进入函数act
-function act() {
+	function act() {
 		//分段重复函数
 		
 		if ((index <= 5 && eNum == 3)) {
@@ -75,7 +67,7 @@ function act() {
 
         }
 
-if (index<comments.length) {					//如果指针在范围内；
+		if (index<comments.length) {			//如果指针在范围内；
 			$(".e_element").hide();				//隐藏所有元素；
 			$(".en").html("");					//初始化英文部分；
 			$(".cn").html("");					//初始化中文部分；
@@ -105,30 +97,31 @@ if (index<comments.length) {					//如果指针在范围内；
 			$(".btnQ").html(btnQ);
 			$(".e_act1").show();				//显示：act1元素；
 			
-			var arrSelect=[];
-			for(var i=0, len=org_comments.length; i<len; i++){
+
+			var arrSelect=[];	//定义select数组；
+			for(var i=0, len=org_comments.length; i<len; i++){	//取出当前指针的值。（同时取出与当前指正重复的值。）
 				if(org_comments[i]['subTitle']!=comments[index]['subTitle']){
 					arrSelect.push(org_comments[i]);
 				}	
 			}
-			arrSelect=randomOrder(arrSelect);
+			arrSelect=randomOrder(arrSelect);	//乱序数组
 			
 
-			var arrSelect2 = [];
-			for(i=0;i<=2;i++){
+			var arrSelect2 = [];	//定时数组2
+
+			var s_num=4;
+			for(i=0;i<=s_num;i++){	//获得目标数组-1的新数组
 				arrSelect2.push(arrSelect[i]);
 			}
-			arrSelect2.push(comments[index]);
-			arrSelect = randomOrder(arrSelect2);
+			arrSelect2.push(comments[index]);	//新数组添加当前指针的值。
+			arrSelect = randomOrder(arrSelect2);	//乱序数组。
 
-			$(".f_btnNo1").html(arrSelect[0].subject);
-			$(".f_btnNo1").attr("f_a",arrSelect[0].subTitle);
-			$(".f_btnNo2").html(arrSelect[1].subject);
-			$(".f_btnNo2").attr("f_a",arrSelect[1].subTitle);
-			$(".f_btnNo3").html(arrSelect[2].subject);
-			$(".f_btnNo3").attr("f_a",arrSelect[2].subTitle);
-			$(".f_btnNo4").html(arrSelect[3].subject);
-			$(".f_btnNo4").attr("f_a",arrSelect[3].subTitle);
+			$(".f_btnSelect").hide();
+			for(i=0;i<=s_num+1;i++){
+				$(".f_btnNo"+(i+1)).html(arrSelect[i].subject); //赋值
+				$(".f_btnNo"+(i+1)).attr("f_a",arrSelect[i].subTitle);
+				$(".f_btnNo"+(i+1)).show();
+			}
 			
 			play3(1);
 //			$("textarea.b")[0].focus();			//输入框焦点
@@ -176,58 +169,10 @@ if (index<comments.length) {					//如果指针在范围内；
 	<?php include("section/v2/s_v2_f_slipt.js"); ?>
 	<?php include("section/v2/s_v2_functions.js"); ?>
 })
-//随机改变数组的排序
-function randomOrder (targetArray){
-    var arrayLength = targetArray.length
-    //
-    //先创建一个正常顺序的数组
-    var tempArray1 = new Array();
 
-    for (var i = 0; i < arrayLength; i ++)
-    {
-        tempArray1 [i] = i
-    }
-    //
-    //再根据上一个数组创建一个随机乱序的数组
-    var tempArray2 = new Array();
 
-    for (var i = 0; i < arrayLength; i ++)
-    {
-        //从正常顺序数组中随机抽出元素
-        tempArray2 [i] = tempArray1.splice (Math.floor (Math.random () * tempArray1.length) , 1)
-    }
-    //
-    //最后创建一个临时数组存储 根据上一个乱序的数组从targetArray中取得数据
-    var tempArray3 = new Array();
 
-    for (var i = 0; i < arrayLength; i ++) {
-        tempArray3 [i] = targetArray [tempArray2 [i]]
-    }
-    
-//
-    //返回最后得出的数组
-    return tempArray3
-}
-
-//使用实例
-
-// var tmp = ["1", "2", "3", "4"];
-// alert(randomOrder(tmp));
-
-	function print_array(arr) {
-var t = 'array(\n';
-for(var key in arr) {
-if(typeof(arr[key]) == 'array' || typeof(arr[key]) == 'object') {
-var t_tmp = key + ' = ' + print_array(arr[key]);
-t += '\t' + t_tmp + '\n';
-} else {
-var t_tmp = key + ' = ' + arr[key];
-t += '\t' + t_tmp + '\n';
-}
-}
-t = t + ')';
-return t;;
-}
+	
 	
 
 
